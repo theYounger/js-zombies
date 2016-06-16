0dd314d65d0d50971f60dbb340d54018449896c3
theYounger <kuyehara1@gmail.com>
2016-06-15 15:30:04 -1000

    RangedZombie class complete: test pass

--- zombies.js
+++ zombies.js
@@ -399,13 +399,16 @@ StrongZombie.prototype = Object.create(Zombie.prototype);
  * @param {number} speed            The zombie's speed.
  */
 
+function RangedZombie(health, strength, speed) {
+  Zombie.call(this, health, strength, speed);
+}
 
 /**
  * StrongZombie Extends Zombie Class
  * -----------------------------
  */
 
-
+RangedZombie.prototype = Object.create(Zombie.prototype);
 
 /**
  * Class => ExplodingZombie(health, strength, speed)
